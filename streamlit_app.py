cb0048a19e1fcc8a595b69972580c8d14137e1d5
melody-10 <melodyhernandez09@gmail.com>
2025-08-30 17:53:18 -0600

    First test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import pandas as pd
-import openpyxl as op
 
 st.title("Proyecto BI")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building!"
 )
 
 df = pd.read_excel("Data.xlsx")
