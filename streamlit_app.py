473031d981da68b1df7f5312a7ce3ef96c472f99
melody-10 <melodyhernandez09@gmail.com>
2025-08-30 15:53:11 -0600

    First_commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import pandas as pd
+import openpyxl as op
 
-st.title("🎈 My new app")
+st.title("Proyecto BI")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+df = pd.read_excel("Data.xlsx")
+df.head()
