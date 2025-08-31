77b0907ec18e918cd66279ca2732bda54330cb90
melody-10 <melodyhernandez09@gmail.com>
2025-08-30 18:04:08 -0600

    Test 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,18 @@
 import streamlit as st
 import pandas as pd
+import os
 
 st.title("Proyecto BI")
 st.write(
     "Let's start building!"
 )
 
-df = pd.read_excel("Datos/Data.xlsx")
-df.head()
+# Asegurar ruta correcta
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(BASE_DIR, "Data.xlsx")
+
+# Leer archivo Excel
+df = pd.read_excel(file_path)
+
+# Mostrar en la app
+st.dataframe(df.head())
